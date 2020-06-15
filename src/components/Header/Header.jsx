6575affe9de78eee58b8e8b83e21525a45e0c32e
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-06-15 14:14:16 -0400

    feat: WIP Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = ({
+  children
+}) => {
+  return (<h1>{children}</h1>);
+}
+
+export default Header;
---
+++ src/components/Header/Header.stories.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Header from './Header.jsx';
+// import palmettoColors from '@palmetto/palmetto-design-tokens/build/js/colors.js';
+// import '../../main.scss';
+// import './colors.scss';
+// import { action } from '@storybook/addon-actions';
+
+// const [baseColors, brandColors] = Object.values(palmettoColors.color);
+
+export default {
+  title: 'Header',
+  component: Header,
+};
+
+// const renderColorBlock = (colorEntry) => {
+//   const [colorName, colorVariations] = colorEntry;
+
+//   return (
+//     <div className="colors__color-block__item" style={{ backgroundColor: `${colorVariations['base'].value}` }}>
+//       <h3>{colorName}</h3>
+//       <p>{colorVariations['base'].value}</p>
+//     </div>
+//   );
+// };
+
+// const renderColorPalette = (colorEntry, index) => {
+//   const [colorName, colorVariations] = colorEntry;
+
+//   const getFontColor = (colorVariation) => {
+//     return colorVariation?.attributes?.font === 'base' ? 'black' : 'white';
+//   }
+
+//   return (
+//     <div key={index}>
+//       <h3 style={{ marginTop: '0' }}>{colorName}</h3>
+//       {Object.entries(colorVariations).map((colorVariationEntry, index) => {
+//         const [colorVariationName, colorVariation] = colorVariationEntry;
+//         return (
+//           <div
+//             key={index}
+//             className="colors__color-palette__item"
+//             style={{ backgroundColor: `${colorVariation.value}`, color: `${getFontColor(colorVariation)}`}}
+//           >
+//             <small style={{ display: 'block' }}>{colorVariationName}</small>
+//             <small>{colorVariation.value}</small>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+export const all = () => (
+  <div style={{ padding: '1rem' }}>
+    <Header>Hello World!</Header>
+  </div>
+);
