88e4655bff446e03c71df2275f677d58dcfd7a0e
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-06-15 16:51:18 -0400

    chore: more headings in storybook

--- src/components/Header/Heading.stories.jsx
+++ src/components/Header/Heading.stories.jsx
@@ -8,6 +8,10 @@ export default {
 
 export const all = () => (
   <div style={{ padding: '1rem' }}>
-    <Heading as="h1" size="2xl">Heading 1</Heading>
+    <Heading as="h1" size="3xl">Heading 1</Heading>
+    <Heading as="h2" size="2xl">Heading 2</Heading>
+    <Heading as="h3" size="xl">Heading 3</Heading>
+    <Heading as="h4" size="lg">Heading 4</Heading>
+    <Heading as="h5" size="md">Heading 5</Heading>
   </div>
 );
