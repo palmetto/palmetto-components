b5eedf80782b1688430db353f16f9b8e16b2114b
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2020-07-06 07:54:38 -0700

    add InputValidaitonMessage story

--- src/components/InputValidationMessage/InputValidationMessage.jsx
+++ src/components/InputValidationMessage/InputValidationMessage.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 const InputValidationMessage = ({
   children,
 }) => (
-    <div className="font-color-danger font-size-sm" style={{ marginTop: 'var(--size-spacing-xs)' }}>{children}</div>
-  );
+  <div className="font-color-danger font-size-sm" style={{ marginTop: 'var(--size-spacing-xs)' }}>{children}</div>
+);
 
 InputValidationMessage.propTypes = {
   children: PropTypes.node.isRequired,
