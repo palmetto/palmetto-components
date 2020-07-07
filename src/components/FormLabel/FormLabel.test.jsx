bfafbff4c06c72e9837a88825d1bfc5ce4d0093e
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-07-07 09:03:14 -0400

    chore: cleanup ids and fragments

--- src/components/FormLabel/FormLabel.test.jsx
+++ src/components/FormLabel/FormLabel.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { v4 as uuid } from 'uuid';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import FormLabel from './FormLabel';
@@ -21,24 +20,24 @@ describe('FormLabel', () => {
   });
 
   test('Throws error if required prop "labelText" is not supplied to component', () => {
-    render(<FormLabel inputId={uuid()} />);
+    render(<FormLabel inputId="myId" />);
     expect(console.error).toHaveBeenCalledTimes(1); // eslint-disable-line no-console
     expect(console.error.mock.calls[0][0]) // eslint-disable-line no-console
       .toContain('Failed prop type: The prop `labelText`');
   });
 
   test('Matches the snapshot for component with default props', () => {
-    const { asFragment } = render(<FormLabel inputId={uuid()} labelText="hello world" />);
-    expect(asFragment(<FormLabel>My label</FormLabel>)).toMatchSnapshot();
+    const { asFragment } = render(<FormLabel inputId="myId" labelText="hello world" />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Matches the snapshot for component with isRequired prop', () => {
-    const { asFragment } = render(<FormLabel inputId={uuid()} labelText="hello world" isRequired />);
-    expect(asFragment(<FormLabel isRequired>Field is required</FormLabel>)).toMatchSnapshot();
+    const { asFragment } = render(<FormLabel inputId="myId" labelText="hello world" isRequired />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Matches the snapshot for component with hasError prop', () => {
-    const { asFragment } = render(<FormLabel inputId={uuid()} labelText="hello world" hasError />);
-    expect(asFragment(<FormLabel hasError>Field has error</FormLabel>)).toMatchSnapshot();
+    const { asFragment } = render(<FormLabel inputId="myId" labelText="hello world" hasError />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
