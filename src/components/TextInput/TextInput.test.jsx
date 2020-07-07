62d95880025aea75bfa632bb4192ce8fa49e696e
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-07-07 11:18:58 -0400

    chore: more text input tests

--- src/components/TextInput/TextInput.test.jsx
+++ src/components/TextInput/TextInput.test.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import TextInput from './TextInput';
 
 describe('TextInput', () => {
+  /**
+   * Props Validation
+   */
   test('Throws error if required prop "value" is not supplied to component', () => {
     console.error = jest.fn(); // eslint-disable-line no-console
     render(<TextInput onChange={() => null} />);
@@ -20,11 +28,18 @@ describe('TextInput', () => {
       .toContain('Failed prop type: The prop `onChange`');
   });
 
-  test('Matches the snapshot for component with default props', () => {
-    const { asFragment } = render(<TextInput value="" onChange={() => null} id="myId" />);
-    expect(asFragment()).toMatchSnapshot();
+  test('Throws an error if "type" prop is anything other than allowed values', () => {
+    console.error = jest.fn(); // eslint-disable-line no-console
+    render(<TextInput onChange={() => null} value="hello" type="notOnTheList" />);
+    expect(console.error).toHaveBeenCalledTimes(1); // eslint-disable-line no-console
+    expect(console.error.mock.calls[0][0]) // eslint-disable-line no-console
+      .toContain('Failed prop type: Invalid prop `type`');
+    // expect()
   });
 
+  /**
+   * Component Functionality
+   */
   test('onChange event fires callback function', () => {
     const onChangeMock = jest.fn(() => null);
 
@@ -89,5 +104,41 @@ describe('TextInput', () => {
     fireEvent.focus(inputElement);
     fireEvent.blur(inputElement);
     expect(mockedOnBlur).toBeCalledTimes(1);
+  });
+
+  test('Input autofocuses if "autoFocus" prop is set to true', () => {
+    render(<TextInput value="hello" onChange={() => null} autoFocus />);
+    const inputElement = screen.getByDisplayValue('hello');
+    expect(document.activeElement).toEqual(inputElement);
+  });
+
+  /**
+   * Snapshots
+   * NOTE: Even though 'id' is not a required prop for this component we must provide it for the test
+   * to pass since the component will otherwise generate a uuid which means the snapshot is different every time.
+   */
+  test('Matches the snapshot for component with default props', () => {
+    const { asFragment } = render(<TextInput value="" onChange={() => null} id="myId" />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('Matches the snapshot for a disabled input', () => {
+    const { asFragment } = render(<TextInput value="" onChange={() => null} id="myId" isDisabled />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('Matches the snapshot for a required input', () => {
+    const { asFragment } = render(<TextInput value="" onChange={() => null} id="myId" isRequired />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('Matches the snapshot for an input with an error but no validation message', () => {
+    const { asFragment } = render(<TextInput value="" onChange={() => null} id="myId" error />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('Matches the snapshot for an input with an error and a validation message', () => {
+    const { asFragment } = render(<TextInput value="" onChange={() => null} id="myId" error="You silly goose" />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
