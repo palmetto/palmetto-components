fcc0b518c3893dca8119aeaef4b4a3c34af03d23
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-07-08 13:37:36 -0400

    feat: adding formik bindings for form fields

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
-import TextInput from './TextInput/TextInput';
-import Heading from './Heading/Heading';
 import CheckboxInput from './CheckboxInput/CheckboxInput';
+import FormikTextInput from './FormikTextInput/FormikTextInput';
+import Heading from './Heading/Heading';
+import TextInput from './TextInput/TextInput';
 
 export {
   CheckboxInput,
-  TextInput,
+  FormikTextInput,
   Heading,
+  TextInput,
 };
