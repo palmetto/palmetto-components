8405b73d6f631d0f952dda1f661335a77f2167e1
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-07-08 13:40:05 -0400

    chore: adding new components to exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import CheckboxInput from './CheckboxInput/CheckboxInput';
+import FormikCheckboxInput from './FormikCheckboxInput/FormikCheckboxInput';
 import FormikTextInput from './FormikTextInput/FormikTextInput';
 import Heading from './Heading/Heading';
 import TextInput from './TextInput/TextInput';
 
 export {
   CheckboxInput,
+  FormikCheckboxInput,
   FormikTextInput,
   Heading,
   TextInput,
