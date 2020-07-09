32c15bec3640d9a96e367fe97a22acf3a3506518
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2020-07-09 08:26:08 -0700

    add component description

--- src/components/SelectInput/SelectInput.jsx
+++ src/components/SelectInput/SelectInput.jsx
@@ -7,6 +7,11 @@ import FormLabel from '../FormLabel/FormLabel';
 import InputValidationMessage from '../InputValidationMessage/InputValidationMessage';
 import './SelectInput.scss';
 
+/**
+ * Allows users to pick a value from predefined list of options.
+ * Ideally, it should be used when there are more than 4 options, otherwise you should consider using a radio group instead.
+ */
+
 const SelectInput = ({
   id,
   label,
