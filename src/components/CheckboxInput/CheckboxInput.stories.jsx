bd6e56e197a779852334d3bf7c2b79681f9a610a
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-07-09 06:54:11 -0400

    checkboxinput stories

--- src/components/CheckboxInput/CheckboxInput.stories.jsx
+++ src/components/CheckboxInput/CheckboxInput.stories.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Store, State } from '@sambego/storybook-state';
 import { action } from '@storybook/addon-actions';
 import { withA11y } from '@storybook/addon-a11y';
 import CheckboxInput from './CheckboxInput';
@@ -10,88 +10,100 @@ export default {
   decorators: [withA11y],
 };
 
-const StatefulCheckboxInput = props => {
-  const { initialCheckedValue, label, isDisabled } = props; // eslint-disable-line
-  const [isCheckboxChecked, setIsCheckboxChecked] = useState(initialCheckedValue);
+export const All = () => {
+  const store = new Store({
+    default: false,
+    required: false,
+    initialChecked: true,
+    withLabelHtml: false,
+    disabledUnchecked: false,
+    disabledChecked: true,
+    withError: false,
+    withErrorRequired: false,
+  });
 
-  const handleChange = isChecked => {
-    action('onChange')(isChecked);
-    setIsCheckboxChecked(isChecked);
+  const handleChange = (value, key) => {
+    action('change')(value);
+    store.set({ [key]: value });
   };
 
   return (
-    <CheckboxInput
-      isChecked={isCheckboxChecked}
-      onChange={handleChange}
-      isDisabled={isDisabled}
-      label={label}
-      {...props} // esli
-    />
+    <State store={store}>
+      {state => (
+        <>
+          <div style={{ marginBottom: '1rem' }}>
+            <CheckboxInput
+              label="Default state is unchecked"
+              isChecked={state.default}
+              onChange={value => handleChange(value, 'default')}
+            />
+          </div>
+          <div style={{ marginBottom: '1rem' }}>
+            <CheckboxInput
+              isRequired
+              label="Required checkbox is unchecked"
+              isChecked={state.required}
+              onChange={value => handleChange(value, 'required')}
+            />
+          </div>
+          <div style={{ marginBottom: '1rem' }}>
+            <CheckboxInput
+              isChecked={state.initialChecked}
+              label="Initial state can be set to checked."
+              onChange={value => handleChange(value, 'initialChecked')}
+            />
+          </div>
+          <div style={{ marginBottom: '1rem' }}>
+            <CheckboxInput
+              label={(
+                <>
+                  <span style={{ color: '#5620c5' }}>
+                    Pass any element(s) as children for the label, even links! Add custom styles.
+                  </span>
+                  {' '}
+                  <a href="https://www.palmetto.com">Go to Palmetto.com</a>
+                </>
+              )}
+              isChecked={state.withLabelHtml}
+              onChange={value => handleChange(value, 'withLabelHtml')}
+            />
+
+          </div>
+          <div style={{ marginBottom: '1rem' }}>
+            <CheckboxInput
+              isDisabled
+              label="Disabled, and unchecked."
+              isChecked={state.disabledUnchecked}
+              onChange={() => null}
+            />
+          </div>
+          <div style={{ marginBottom: '1rem' }}>
+            <CheckboxInput
+              label="Disabled, and checked."
+              isDisabled
+              isChecked={state.disabledChecked}
+              onChange={() => null}
+            />
+          </div>
+          <div style={{ marginBottom: '1rem' }}>
+            <CheckboxInput
+              error="You must accept the Terms and Conditions"
+              label="Invalid checkbox"
+              isChecked={state.withError}
+              onChange={value => handleChange(value, 'withError')}
+            />
+          </div>
+          <div style={{ marginBottom: '1rem' }}>
+            <CheckboxInput
+              isRequired
+              error="You must accept the Terms and Conditions"
+              label="Required Invalid checkbox"
+              isChecked={state.withErrorRequired}
+              onChange={value => handleChange(value, 'withErrorRequired')}
+            />
+          </div>
+        </>
+      )}
+    </State>
   );
 };
-
-StatefulCheckboxInput.defaultProps = {
-  initialCheckedValue: undefined,
-  children: undefined,
-  isDisabled: false,
-};
-
-StatefulCheckboxInput.propTypes = {
-  initialCheckedValue: PropTypes.bool,
-  children: PropTypes.node,
-  isDisabled: PropTypes.bool,
-};
-
-export const All = () => {
-  const firstCheckboxState = true;
-  const secondCheckboxState = true;
-  return (
-    <div>
-      <div style={{ marginBottom: '1rem' }}>
-        <StatefulCheckboxInput
-          label="Default state is unchecked"
-        />
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <StatefulCheckboxInput
-          isRequired
-          label="Required checkbox is unchecked"
-        />
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <StatefulCheckboxInput initialCheckedValue={firstCheckboxState} label="Initial state can be set to checked." />
-
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <StatefulCheckboxInput label={(
-          <>
-            <span style={{ color: '#5620c5' }}>
-              Pass any element(s) as children for the label, even links! Add custom styles.
-            </span>
-            {' '}
-            <a href="https://www.palmetto.com">Go to Palmetto.com</a>
-          </>
-        )}
-        />
-
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <StatefulCheckboxInput isDisabled label="Disabled, and unchecked." />
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <StatefulCheckboxInput isDisabled initialCheckedValue={secondCheckboxState} label="Disabled, and unchecked." />
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <StatefulCheckboxInput error="You must accept the Terms and Conditions" label="Invalid checkbox" />
-      </div>
-      <div style={{ marginBottom: '1rem' }}>
-        <StatefulCheckboxInput
-          isRequired
-          error="You must accept the Terms and Conditions"
-          label="Required Invalid checkbox"
-        />
-      </div>
-    </div>
-  );
-};
-
--- src/components/TextInput/TextInput.stories.jsx
+++ src/components/TextInput/TextInput.stories.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { State, Store } from "@sambego/storybook-state";
 import { action } from '@storybook/addon-actions';
 import { withA11y } from '@storybook/addon-a11y';
