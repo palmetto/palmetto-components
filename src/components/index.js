ddf83cb405ec9a841a75e58e6d854473ebb40e00
Jon Gamble <jon.gamble@palmettoventures.com>
2020-07-17 15:36:39 -0400

    adding missed export from index

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Button from './Button/Button';
 import CheckboxInput from './CheckboxInput/CheckboxInput';
 import FormikCheckboxInput from './Formik/FormikCheckboxInput/FormikCheckboxInput';
+import FormikSelectInput from './Formik/FormikTextInput/FormikSelectInput';
 import FormikTextInput from './Formik/FormikTextInput/FormikTextInput';
 import Heading from './Heading/Heading';
 import TextInput from './TextInput/TextInput';
@@ -10,6 +11,7 @@ export {
   Button,
   CheckboxInput,
   FormikCheckboxInput,
+  FormikSelectInput,
   FormikTextInput,
   Heading,
   TextInput,
