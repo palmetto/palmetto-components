70c6a166aa26c1bdd4ac03e79e077dc6bd894beb
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-07-23 15:59:00 -0400

    consistent code style

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import styles from './Spinner.module.scss';
 import { PALMETTO_BRAND_COLOR_OPTIONS, PALMETTO_BRAND_COLOR_VALUES, PALMETTO_COLORS } from '../../lib/tokens';
+import styles from './Spinner.module.scss';
 
 interface Props {
   /**
