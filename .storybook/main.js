3d74f01942a0338c78253873e4ff636da1d2df31
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-08-03 07:01:53 -0400

    removing comments

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,6 @@
 const webpackConfigDev = require('../webpack.dev');
 const path = require('path');
 
-// const svgRules = config => {
-//   const assetRule = config.module.rules.find(({ test }) => test.test(".svg"));
-//   const assetLoader = {
-//     loader: assetRule.loader,
-//     options: assetRule.options || assetRule.query
-//   };
-//   // Merge our rule with existing assetLoader rules
-//   config.module.rules.unshift({
-//     test: /\.svg$/,
-//     use: ["@svgr/webpack", assetLoader]
-//   });
-//   return config;
-// }
-
 module.exports = {
   stories: ['../src/**/*.stories.([tj]sx|mdx)'],
   addons: [
@@ -22,7 +8,6 @@ module.exports = {
     '@storybook/addon-a11y/register',
     '@storybook/addon-docs',
     '@storybook/addon-links/register',
-    // '@storybook/addon-viewport/register',
   ],
   webpackFinal: (config) => {
     return {
