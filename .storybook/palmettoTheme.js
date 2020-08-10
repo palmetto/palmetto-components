fff8e62be1b0a097bde1e64c4a286baa289719c7
Jesse Carmine <jesse.carmine@palmettoventures.com>
2020-08-10 09:09:02 -0700

    Flip back to light theme.

--- .storybook/palmettoTheme.js
+++ .storybook/palmettoTheme.js
@@ -1,7 +1,7 @@
 import { create } from '@storybook/theming/create';
 
 export default create({
-  base: 'dark',
+  base: 'light',
 
   // colorPrimary: 'hotpink',
   // colorSecondary: 'deepskyblue',
