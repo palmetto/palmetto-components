c2267e3c228780ee5ee428f2454cd86c1d85f3e7
Jesse Carmine <jesse.carmine@palmettoventures.com>
2020-08-10 16:06:08 -0700

    Remove config.js for storybook. Styles being loaded in it were relevant to preview; so added to preview.js.

--- .storybook/config.js
+++
@@ -1,3 +0,0 @@
-import '!style-loader!css-loader!sass-loader!../src/styles/utilities.scss';
-import '!style-loader!css-loader!sass-loader!../src/styles/variables/index.scss';
-import '!style-loader!css-loader!sass-loader!../src/styles/reset.scss';
