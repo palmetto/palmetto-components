cb7e09b6517cd5fc52270d5a4b9a1e9683ae4701
Jesse Carmine <jesse.carmine@palmettoventures.com>
2020-08-12 18:42:35 -0700

    Get initial render in Storybook.

--- src/components/Table/TableHead/TableHead.jsx
+++ src/components/Table/TableHead/TableHead.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-import TableHeaderSortable from 'components/common/TableHeaderSortable/TableHeaderSortable';
-
+import TableHeaderSortable from './TableHeaderSortable/TableHeaderSortable';
 import styles from './TableHead.module.scss';
 
-export const TableHead = ({ columns, sortedColumn, setNewSortedColumn, loading, setPage, useSortedColumns }) => {
-  const handleTableHeaderClick = (clickedColumnName) => {
+export const TableHead = ({ 
+  columns, 
+  sortedColumn, 
+  setNewSortedColumn, 
+  loading, 
+  setPage, 
+  useSortedColumns 
+}) => {
+  const handleTableHeaderClick = clickedColumnName => {
     let direction;
 
     if (clickedColumnName !== sortedColumn.columnName) {
@@ -17,15 +21,15 @@ export const TableHead = ({ columns, sortedColumn, setNewSortedColumn, loading, 
 
     const newSortedColumn = {
       columnName: clickedColumnName,
-      sortOrder: direction
-    }
+      sortOrder: direction,
+    };
 
     setNewSortedColumn(newSortedColumn);
     setPage(1);
-  }
+  };
 
   const renderSortableColumns = () => (
-    columns.map((column) => (
+    columns.map(column => (
       <TableHeaderSortable
         header={column.name}
         queryParam={column.apiKey}
@@ -38,43 +42,20 @@ export const TableHead = ({ columns, sortedColumn, setNewSortedColumn, loading, 
   );
 
   const renderFixedColumns = () => (
-    columns.map((column) => <th className={styles.header} key={column.name} style={{'width': `${column.width}px`}}>{column.name}</th>)
+    columns.map(column => <th className={styles.header} key={column.name} style={{ width: `${column.width}px` }}>{column.name}</th>)
   );
 
   return (
     <thead className={styles.tableHead}>
       <tr>
-        {useSortedColumns
-          ? renderSortableColumns()
-          : renderFixedColumns()
+        {
+          useSortedColumns
+            ? renderSortableColumns()
+            : renderFixedColumns()
         }
       </tr>
     </thead>
-  )
-};
-
-TableHead.defaultProps = {
-  sortedColumn: null,
-  setNewSortedColumn: null,
-  loading: null,
-  setPage: null,
-  useSortedColumns: null
-}
-
-TableHead.propTypes = {
-  columns: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    apiKey: PropTypes.string,
-    width: PropTypes.string
-  })).isRequired,
-  sortedColumn: PropTypes.shape({
-    columnName: PropTypes.string.isRequired,
-    sortOrder: PropTypes.string.isRequired
-  }),
-  setNewSortedColumn: PropTypes.func,
-  loading: PropTypes.bool,
-  setPage: PropTypes.func,
-  useSortedColumns: PropTypes.bool
+  );
 };
 
 export default TableHead;
--- src/components/Table/TableHead/TableHeaderSortable/TableHeaderSortable.jsx
+++ src/components/Table/TableHead/TableHeaderSortable/TableHeaderSortable.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import cx from 'classnames';
-import { FaArrowDown, FaArrowUp, FaSort } from 'react-icons/fa';
-
+import classNames from 'classnames';
 import styles from './TableHeaderSortable.module.scss';
 
-
-export const TableHeaderSortable = ({ header, queryParam, sortedColumn, onSort, loading, width }) => {
+export const TableHeaderSortable = ({
+  header,
+  queryParam,
+  sortedColumn,
+  onSort,
+  loading,
+  width
+}) => {
   const renderIcon = (sortedColumnName, sortColumnOrder, name) => {
     const sortDirection = (sortedColumnName === name) ? sortColumnOrder : null;
 
     return (
       <span>
-        {sortDirection ? (sortDirection === 'desc' ? <FaArrowDown/> : <FaArrowUp/>): <FaSort className={styles.sortIcon}/>}
+        {sortDirection ? (sortDirection === 'desc' ? 'down' : 'up'): 'sort'}
       </span>
     );
   };
@@ -25,18 +29,26 @@ export const TableHeaderSortable = ({ header, queryParam, sortedColumn, onSort, 
     }
   };
 
+  const tableHeaderSortableClasses = classNames(
+    styles.header,
+    {
+      [styles.disabled]: loading,
+    },
+  );
+
   return (
     <th
-      className={ cx(styles.header, {[styles.disabled]: loading}) }
-      style={{'width': `${width}px`}}
+      className={tableHeaderSortableClasses}
+      style={{ width: `${width}px` }}
       role="button"
       tabIndex={0}
       onClick={() => onSort(queryParam)}
       onKeyDown={handleKeyPress}
     >
-      {header} {renderIcon(sortedColumn.columnName, sortedColumn.sortOrder, queryParam)}
+      {header}
+      {renderIcon(sortedColumn.columnName, sortedColumn.sortOrder, queryParam)}
     </th>
-  )
+  );
 };
 
 export default TableHeaderSortable;
