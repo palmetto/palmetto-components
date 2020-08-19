a1d36ab3c9a6d611978dde521e353c08740db77a
Jesse Carmine <jesse.carmine@palmettoventures.com>
2020-08-19 10:18:48 -0700

    Add tests for TableHeaderSortable.

---
+++ src/components/Table/TableHead/TableHeaderSortable/TableHeaderSortable.test.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import {
+  render,
+  fireEvent,
+  screen,
+} from '@testing-library/react';
+import TableHeaderSortable from './TableHeaderSortable';
+
+const mockHandleSort = jest.fn();
+
+describe('TableHead', () => {
+  describe('Callback Handling', () => {
+    describe('onSort', () => {
+      test('onSort event fires callback function', () => {
+        render(
+          <TableHeaderSortable
+            heading="ID"
+            id="first"
+            onSort={mockHandleSort}
+          />,
+        );
+
+        const sortableColumnHeader = screen.getByText('ID');
+
+        fireEvent.click(sortableColumnHeader);
+        expect(mockHandleSort).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+
+  describe('States', () => {
+    describe('Default', () => {
+      test('it renders a sort icon', () => {
+        render(
+          <TableHeaderSortable
+            heading="ID"
+            id="first"
+            onSort={mockHandleSort}
+          />,
+        );
+
+        const sortIcon = screen.getByTestId('sort-testid');
+
+        expect(sortIcon).toBeInTheDocument();
+      });
+    });
+
+    describe('Sorted Descending', () => {
+      test('it renders a descending sort icon', () => {
+        render(
+          <TableHeaderSortable
+            heading="ID"
+            id="first"
+            onSort={mockHandleSort}
+            sortedColumn={{ id: 'first', sortOrder: 'desc' }}
+          />,
+        );
+
+        const sortIcon = screen.getByTestId('arrowDown-testid');
+
+        expect(sortIcon).toBeInTheDocument();
+      });
+    });
+
+    describe('Sorted Ascending', () => {
+      test('it renders a ascending sort icon', () => {
+        render(
+          <TableHeaderSortable
+            heading="ID"
+            id="first"
+            onSort={mockHandleSort}
+            sortedColumn={{ id: 'first', sortOrder: 'asc' }}
+          />,
+        );
+
+        const sortIcon = screen.getByTestId('arrowUp-testid');
+
+        expect(sortIcon).toBeInTheDocument();
+      });
+    });
+
+    // describe('Disabled', () => {
+    //   test('clicking on the header does nothing', () => {
+    //     render(
+    //       <TableHeaderSortable
+    //         heading="ID"
+    //         id="first"
+    //         onSort={mockHandleSort}
+    //         isLoading
+    //       />,
+    //     );
+
+    //     const sortableColumnHeader = screen.getByText('ID');
+
+    //     fireEvent.click(sortableColumnHeader);
+    //     expect(mockHandleSort).toHaveBeenCalledTimes(0);
+    //   });
+    // });
+  });
+});
