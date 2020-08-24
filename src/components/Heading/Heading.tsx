91739c21540216f3042c0aefb69e606de9398a83
Jesse Carmine <jesse.carmine@palmettoventures.com>
2020-08-23 22:22:53 -0700

    Modernize Heading.

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,4 +1,4 @@
-import React, { createElement, FC } from 'react';
+import React, { FC, createElement } from 'react';
 import classNames from 'classnames';
 import getElementType from '../../lib/getElementType';
 import {
@@ -8,44 +8,35 @@ import {
 import { HEADING_LEVELS_TYPE, HEADING_DEFAULT_SIZE_MAP } from './Heading.constants';
 import styles from './Heading.module.scss';
 
-/**
- * Use `Headings` as labels for pages or sections of a page that make up an interface.
- * `Headings` can label an entire page, or sections of related content.
- *
- * The `Heading` component allows you to specify an appropriate semantic element (h1-h6)
- * and independently set its size so that it is appropriate for the surrounding content.
- * If no size is specified, a default size will be applied.
- */
-
-interface Props {
+interface HeadingProps {
   /**
    * The DOM tag or react component to use for the element.
    * Select the appropriate semantic element (h1-h6).
    */
   as?: HEADING_LEVELS_TYPE;
+  children?: React.ReactNode;
   /**
-   * Additional class names to add
+   * Additional class names to add.
    */
   className?: string;
   /**
    * A color token identifier to use for the text color. Available colors found:
-   * [here](https://github.com/palmetto/palmetto-design-tokens/blob/develop/properties/color/font.json)
+   * [here](https://github.com/palmetto/palmetto-design-tokens/blob/develop/properties/color/font.json).
    */
   color?: PALMETTO_FONT_COLORS;
   /**
    * By default, size is determined by the chosen tag (e.g. h1 is bigger than h2).
    * However, size can be set independently so that its size is appropriate for the surrounding content.
    * Available sizes found:
-   * [here](https://github.com/palmetto/palmetto-design-tokens/blob/develop/properties/size/font.json)
+   * [here](https://github.com/palmetto/palmetto-design-tokens/blob/develop/properties/size/font.json).
    */
   size?: PALMETTO_FONT_SIZES;
-  children?: React.ReactNode;
 }
 
-const Heading: FC<Props> = ({
+const Heading: FC<HeadingProps> = ({
   as = 'h4',
-  className,
   children,
+  className,
   color,
   size,
 }) => {
