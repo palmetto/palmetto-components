7636b703dce6e17bab81f8dc759e130034a9bcaa
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-09-03 20:50:02 -0400

    color variations

--- src/components/Alert/Alert.test.tsx
+++ src/components/Alert/Alert.test.tsx
@@ -3,13 +3,34 @@ import { render, screen } from '@testing-library/react';
 import Alert from './Alert';
 
 describe('Alert', () => {
-  describe('States', () => {
+  describe('Default', () => {
     test('It renders an alert with a simple text message', () => {
       const message = 'Hello world!';
       render(<Alert message={message} />);
 
       const alert = screen.getByText(message);
       expect(alert).toBeInTheDocument();
+    });
+  });
+
+  describe('Types', () => {
+    test('It renders with specific classes based on the type prop', () => {
+      const message = 'Hello world!';
+      const types = [
+        'info',
+        'success',
+        'warning',
+        'danger',
+      ];
+
+      const { rerender } = render(<Alert message={message} />);
+
+      types.forEach(type => {
+        rerender(<Alert message={message} type={type} />);
+        const alert = screen.getByText(message);
+        expect(alert).toBeInTheDocument();
+        expect(alert).toHaveClass(type);
+      });
     });
   });
 });
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -6,13 +6,19 @@ interface AlertProps {
   /**
    * The text message or ReactNode to be rendered in the alert.
    */
-  message: string | ReactNode,
+  message: string | ReactNode;
+  /**
+   * The type/color of the alert to show.
+   */
+  type: 'info' | 'success' | 'warning' | 'danger';
 }
 const Alert: FC<AlertProps> = ({
   message,
+  type = undefined,
 }) => {
   const alertClasses = classNames(
     styles.alert,
+    { [type ? styles[type] : null]: !!type },
   );
 
   return (
