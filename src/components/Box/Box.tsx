e1b677f562589920e81cfaefacf1907d49c1a525
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2020-09-03 18:28:47 -0700

    wip

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -28,6 +28,15 @@ function getSpacingCss(attribute, value) {
     if (side.length === 2) {
       if (side[0] !== '0') classes.push(`${attribute}-v-${side[0]}`);
       if (side[1] !== '0') classes.push(`${attribute}-h-${side[1]}`);
+    } else if (side.length === 3) { // top, horizontal, bottom
+      if (side[0] !== '0') classes.push(`${attribute}-top-${side[0]}`);
+      if (side[1] !== '0') classes.push(`${attribute}-h-${side[1]}`);
+      if (side[2] !== '0') classes.push(`${attribute}-bottom-${side[2]}`);
+    } else if (side.length === 4) { // top, right, bottom left
+      if (side[0] !== '0') classes.push(`${attribute}-top-${side[0]}`);
+      if (side[1] !== '0') classes.push(`${attribute}-right-${side[1]}`);
+      if (side[2] !== '0') classes.push(`${attribute}-bottom-${side[2]}`);
+      if (side[3] !== '0') classes.push(`${attribute}-left-${side[3]}`);
     }
   } else if (typeof value === 'string') {
     if (value === 'inherit') {
