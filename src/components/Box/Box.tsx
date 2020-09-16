52db6aac5a184a87fe288856205934d198982bed
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-09-16 10:57:49 -0400

    remove react import

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,4 +1,4 @@
-import React, { createElement, FC, ReactNode } from 'react';
+import { createElement, FC, ReactNode } from 'react';
 import classNames from 'classnames';
 import {
   PALMETTO_FONT_SIZES,
