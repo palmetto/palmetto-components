bdbeaf2ab479cd39098a6fca4b7b7cbc4ca4a50e
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-09-16 10:30:03 -0400

    refactor dimension css

--- src/lib/getDimensionCss.ts
+++ src/lib/getDimensionCss.ts
@@ -1,39 +1,42 @@
 import { CssObjectType, DimensionType } from './types';
 
+function doesValueIncludeCssUnit(value: string): boolean {
+  const cssUnits = ['px', 'em', 'rem', '%', 'vw', 'vh'];
+
+  return cssUnits.some(unit => value.includes(unit));
+}
+
+function getDimensionStyles(dimension: DimensionType, value?: string): { [key: string]: string; } | undefined {
+  if (value === undefined) return value;
+
+  let styles;
+  // value is a css unit so set its style property
+  if (typeof value === 'string' && doesValueIncludeCssUnit(value)) {
+    styles = dimension === 'w' ? { width: value } : { height: value };
+  }
+
+  return styles;
+}
+
+function getDimensionClasses(dimension: DimensionType, value?: string): string[] | undefined {
+  if (value === undefined) return value;
+
+  const classes = [];
+  if (typeof value === 'string' && !doesValueIncludeCssUnit(value)) {
+    classes.push(`${dimension}-${value}`);
+  }
+
+  return classes;
+}
 /**
  * Returns an object of styles and class names that correspond with the given value
  * @param {DimensionType} dimension width or height
  * @param {string} [value] value of the dimension
  */
 function getDimensionCss(dimension: DimensionType, value?: string): CssObjectType {
-  if (value === undefined) {
-    return {
-      styles: {},
-      classes: [],
-    };
-  }
-
-  const classes = [];
-  let styles;
-
-  // value is a css unit so set its style property
-  if (
-    typeof value === 'string'
-    && (value.includes('px')
-      || value.includes('em')
-      || value.includes('rem')
-      || value.includes('%')
-      || value.includes('vw')
-      || value.includes('vh'))
-  ) {
-    styles = dimension === 'w' ? { width: value } : { height: value };
-  } else if (typeof value === 'string') { // standard percentage or token value
-    classes.push(`${dimension}-${value}`);
-  }
-
   return ({
-    styles,
-    classes,
+    styles: getDimensionStyles(dimension, value),
+    classes: getDimensionClasses(dimension, value),
   });
 }
 
