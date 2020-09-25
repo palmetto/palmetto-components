4aefa6aece46f5ee8b7eda4b9b872b98e4150a83
Jesse Carmine <carminej@gmail.com>
2020-09-25 11:13:01 -0700

    fix: Bug Fixes (#250)
    * Fix fontsize bug.
    
    * Fix spacing story bug.
    
    * Update exports.
    
    * Get alert working correctly.
    
    * Only target direct descendent paragraph.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,31 +1,31 @@
+import Alert from './Alert/Alert';
 import Box from './Box/Box';
 import Button from './Button/Button';
 import CheckboxInput from './CheckboxInput/CheckboxInput';
 import FormikCheckboxInput from './Formik/FormikCheckboxInput/FormikCheckboxInput';
+import FormikRadioGroup from './Formik/FormikRadioGroup/FormikRadioGroup';
 import FormikSelectInput from './Formik/FormikSelectInput/FormikSelectInput';
 import FormikTextInput from './Formik/FormikTextInput/FormikTextInput';
 import Heading from './Heading/Heading';
-import TextInput from './TextInput/TextInput';
-import SelectInput from './SelectInput/SelectInput';
 import RadioGroup from './RadioGroup/RadioGroup';
+import SelectInput from './SelectInput/SelectInput';
 import Table from './Table/Table';
-import TableHead from './Table/TableHead/TableHead';
-import TableBody from './Table/TableBody/TableBody';
-import TableRow from './Table/TableRow/TableRow';
+import TextInput from './TextInput/TextInput';
+import TextLink from './TextLink/TextLink';
 
 export {
+  Alert,
   Box,
   Button,
   CheckboxInput,
   FormikCheckboxInput,
+  FormikRadioGroup,
   FormikSelectInput,
   FormikTextInput,
   Heading,
-  TextInput,
-  SelectInput,
   RadioGroup,
+  SelectInput,
   Table,
-  TableHead,
-  TableBody,
-  TableRow,
+  TextInput,
+  TextLink,
 };
