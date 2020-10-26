7b8d598131da20f741f39312965dfe49e83f77e6
Juan Fabrega <juanantoniofabrega@gmail.com>
2020-10-26 10:20:48 -0400

    updating storybook

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,4 +1,11 @@
-import React, { FC, useRef, ReactNode } from 'react';
+import React, {
+  cloneElement,
+  FC,
+  useRef,
+  useState,
+  ReactNode,
+  ReactElement,
+} from 'react';
 import { usePopper } from 'react-popper';
 
 interface PopoverProps {
@@ -13,19 +20,30 @@ const Popover: FC<PopoverProps> = ({
 }) => {
   const buttonRef = useRef(null);
   const popperRef = useRef(null);
+  const [arrowRef, setArrowRef] = useState<HTMLDivElement | null>(null);
   const { styles, attributes } = usePopper(
     buttonRef.current,
     popperRef.current,
+    { modifiers: [{ name: 'arrow', options: { element: arrowRef } }] },
   );
+
+  const childrenWithRef = React.Children.map(children, child => {
+    // checking isValidElement is the safe way and avoids a typescript error too
+    const props = { ref: buttonRef };
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, props);
+      console.log('did I clone?');
+    }
+    return child;
+  });
 
   return (
     <>
-      <span ref={buttonRef}>
-        {children}
-      </span>
+      {childrenWithRef}
       {isOpen && (
-        <div ref={popperRef} style={styles.popper} {...attributes} data-popper-placement="bottom">
+        <div ref={popperRef} style={styles.popper} {...attributes}>
           {render && render()}
+          <div ref={setArrowRef} style={styles.arrow} />
         </div>
       )}
     </>
