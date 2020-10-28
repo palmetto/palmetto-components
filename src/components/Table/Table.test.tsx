eddf44a84847a496bb9a1fa3c3a2ab29bac03597
Jesse Carmine <carminej@gmail.com>
2020-10-28 10:41:17 -0700

    Fix broken tsbuild.

--- src/components/Table/Table.test.tsx
+++ src/components/Table/Table.test.tsx
@@ -394,7 +394,7 @@ describe('Table', () => {
     describe('Sticky Column', () => {
       test('if a column is sticky, it renders the row header elements', () => {
         const columnConfigStickyColumn = [
-          { heading: 'ID', dataKey: 'id', sticky: 'left' },
+          { heading: 'ID', dataKey: 'id', sticky: 'left' as const },
           { heading: 'Color', dataKey: 'color' },
           { heading: 'Flavor', dataKey: 'flavor' },
         ];
