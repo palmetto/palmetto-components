28f39f686f19fa8c823de02e8724271f27f79a04
Eric Osborne <osbornedesign@yahoo.com>
2020-11-18 16:27:22 -0500

    feat(Badge): badge component
    * Added Badge files
    
    * added badge component
    
    Includes:
    - variants
    - sizes
    - custom class
    
    * badge component
    
    * eslint js fixes
    
    * linter issue
    
    * Updating the badge documentation
    
    * fixed issues in Badge stories
    
    * fixes to badge component
    
    * added badge tests
    
    Co-authored-by: Juan Fabrega <juanantoniofabrega@gmail.com>
    Co-authored-by: Nathan Young <1447339+nathanyoung@users.noreply.github.com>

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,89 @@
+import React, {
+  FC,
+} from 'react';
+import classNames from 'classnames';
+import {
+  BrandColor,
+  FontColor,
+  FontSize,
+  BaseSpacing,
+} from '../../types';
+import styles from './Badge.module.scss';
+import Box from '../Box/Box';
+
+export type BadgeSize = 'sm' | 'md' | 'lg' | 'xl';
+export type BadgeVariant = 'info' | 'primary' | 'success' | 'secondary' | 'tertiary' | 'warning' | 'danger' | 'default';
+export type BadgeColorAttributes = { font: FontColor; background: BrandColor; };
+export type BadgeSizeAttributes = { fontSize: FontSize; padding: BaseSpacing; };
+
+interface BadgeProps {
+  /**
+   * Custom class to apply to the badge container div.
+   */
+  className?: string;
+  /**
+   * The text message or ReactNode to be rendered in the badge.
+   */
+  message?: string;
+  /**
+   * The size of the button.
+   */
+  size?: BadgeSize;
+  /**
+   * The type/color of the badge to show.
+   */
+  variant?: BadgeVariant;
+  /**
+   * Additional props to be spread to rendered element
+   */
+  [x: string]: any; // eslint-disable-line
+}
+
+const Badge: FC<BadgeProps> = ({
+  className = '',
+  message = '',
+  variant = 'default',
+  size = 'md',
+  ...restProps
+}) => {
+  const badgeClasses: string = classNames(
+    styles.badge,
+    className,
+  );
+
+  const colorMap: { [key in BadgeVariant]: BadgeColorAttributes } = {
+    info: { font: 'dark-500', background: 'info-100' },
+    primary: { font: 'dark-500', background: 'primary-100' },
+    success: { font: 'dark-500', background: 'success-100' },
+    secondary: { font: 'dark-500', background: 'secondary-100' },
+    warning: { font: 'dark-500', background: 'warning-100' },
+    tertiary: { font: 'dark-500', background: 'tertiary-100' },
+    danger: { font: 'dark-500', background: 'danger-100' },
+    default: { font: 'dark-500', background: 'grey-100' },
+  };
+
+  const sizeMap: { [key in BadgeSize]: BadgeSizeAttributes} = {
+    sm: { fontSize: '2xs', padding: '2xs 2xs' },
+    md: { fontSize: 'xs', padding: '2xs xs' },
+    lg: { fontSize: 'sm', padding: '2xs xs' },
+    xl: { fontSize: 'md', padding: 'xs sm' },
+  };
+
+  return (
+    <Box
+      className={badgeClasses}
+      display="inline-block"
+      radius="sm"
+      background={colorMap[variant].background}
+      color={colorMap[variant].font}
+      fontWeight="bold"
+      fontSize={sizeMap[size].fontSize}
+      padding={sizeMap[size].padding}
+      {...restProps}
+    >
+      {message}
+    </Box>
+  );
+};
+
+export default Badge;
