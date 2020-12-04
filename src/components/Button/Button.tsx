ea77c1465db5c7aaf95ebf0d4960e7ed2063e266
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2020-12-03 19:21:01 -0800

    feat(Button): prefix and suffix icons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,8 @@
-import React, {
-  FC,
-  ReactNode,
-  MouseEvent,
-  FocusEvent,
-  forwardRef,
-  createElement,
-} from 'react';
+import React, { FC, ReactNode, MouseEvent, FocusEvent, forwardRef, createElement } from 'react';
 import classNames from 'classnames';
+import { IconName } from '../../types';
+import Box from '../Box/Box';
+import Icon from '../Icon/Icon';
 import getElementType from '../../lib/getElementType';
 import Spinner from '../Spinner/Spinner';
 import styles from './Button.module.scss';
@@ -31,6 +27,14 @@ interface ButtonProps {
    * Make button take full width of container.
    */
   fullWidth?: boolean;
+  /**
+   * Name of the icon to include before the button text
+   */
+  iconPrefix?: IconName;
+  /**
+   * Name of the icon to include after the button text
+   */
+  iconSuffix?: IconName;
   /**
    * A unique identifier for the button.
    */
@@ -92,87 +96,89 @@ interface ButtonProps {
   [x: string]: any; // eslint-disable-line
 }
 
-const Button: FC<ButtonProps> = forwardRef((
-  {
-    children = null,
-    as = 'button',
-    className = '',
-    fullWidth = false,
-    id = undefined,
-    href = undefined,
-    isDisabled = false,
-    isLoading = false,
-    isOutlined = false,
-    isNaked = false,
-    tabIndex = undefined,
-    type = 'button',
-    onClick = undefined,
-    onFocus = undefined,
-    onBlur = undefined,
-    size = 'md',
-    variant = 'primary',
-    ...restProps
-  },
-  ref,
-) => {
-  const disabled = isLoading || isDisabled;
-
-  const buttonClasses = classNames(
-    styles.button,
-    className,
+const Button: FC<ButtonProps> = forwardRef(
+  (
     {
+      children = null,
+      as = 'button',
+      className = '',
+      fullWidth = false,
+      id = undefined,
+      href = undefined,
+      iconPrefix = undefined,
+      iconSuffix = undefined,
+      isDisabled = false,
+      isLoading = false,
+      isOutlined = false,
+      isNaked = false,
+      tabIndex = undefined,
+      type = 'button',
+      onClick = undefined,
+      onFocus = undefined,
+      onBlur = undefined,
+      size = 'md',
+      variant = 'primary',
+      ...restProps
+    },
+    ref,
+  ) => {
+    const disabled = isLoading || isDisabled;
+
+    const buttonClasses = classNames(styles.button, className, {
       [styles.outline]: isOutlined && !isNaked,
       [styles.loading]: isLoading,
       [styles.naked]: isNaked,
       [styles[variant]]: variant && !isNaked,
       [styles[size]]: size && !isNaked,
       [styles['full-width']]: fullWidth,
-    },
-  );
+    });
 
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    if (onClick) onClick(event);
-  };
+    const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+      if (onClick) onClick(event);
+    };
 
-  const handleFocus = (event: FocusEvent<HTMLButtonElement | HTMLAnchorElement>) => {
-    if (onFocus) onFocus(event);
-  };
+    const handleFocus = (event: FocusEvent<HTMLButtonElement | HTMLAnchorElement>) => {
+      if (onFocus) onFocus(event);
+    };
 
-  const handleBlur = (event: FocusEvent<HTMLButtonElement | HTMLAnchorElement>) => {
-    if (onBlur) onBlur(event);
-  };
+    const handleBlur = (event: FocusEvent<HTMLButtonElement | HTMLAnchorElement>) => {
+      if (onBlur) onBlur(event);
+    };
 
-  const getSpinnerVariant = () => {
-    if (isOutlined) return variant;
+    const getSpinnerVariant = () => {
+      if (isOutlined) return variant;
 
-    return variant === 'light' ? 'grey' : 'white';
-  };
+      return variant === 'light' ? 'grey' : 'white';
+    };
 
-  const buttonContent = (
-    <>
-      {isLoading && (
-        <Spinner variant={getSpinnerVariant()} className={styles['spinner-wrapper']} />
-      )}
-      <span className={styles.label}>{children}</span>
-    </>
-  );
+    const buttonContent = (
+      <Box display="inline-flex" direction="row" alignItems="center" childGap="xs">
+        {isLoading && (
+          <Spinner variant={getSpinnerVariant()} className={styles['spinner-wrapper']} />
+        )}
+        {iconPrefix && <Icon name={iconPrefix} />}
+        {children && <span className={styles.label}>{children}</span>}
+        {iconSuffix && <Icon name={iconSuffix} />}
+      </Box>
+    );
 
-  const buttonElement = getElementType(Button, { as });
+    const buttonElement = getElementType(Button, { as });
 
-  return createElement(buttonElement, {
-    id,
-    href,
-    className: buttonClasses,
-    ...children && { children: buttonContent },
-    disabled,
-    onBlur: handleBlur,
-    onClick: handleClick,
-    onFocus: handleFocus,
-    ref,
-    type,
-    tabIndex,
-    ...restProps,
-  });
-});
+    return createElement(buttonElement, {
+      id,
+      href,
+      className: buttonClasses,
+      ...{ children: buttonContent },
+      disabled,
+      onBlur: handleBlur,
+      onClick: handleClick,
+      onFocus: handleFocus,
+      ref,
+      type,
+      tabIndex,
+      ...restProps,
+    });
+  },
+);
 
 export default Button;
