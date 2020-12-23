da4c1907782d28fe29cf95d471aa852031d8bcac
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2020-12-23 14:50:54 -0800

    Update Button.stories.tsx

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,52 +1,23 @@
-import React, { ReactElement } from 'react';
-import Button from './Button';
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import Button, { ButtonProps } from './Button';
 import Box from '../Box/Box';
 
 export default {
   title: 'Components/Button/For Chromatic',
-  description: 'stories for visual regression testing',
   component: Button,
-};
+} as Meta;
 
 const buttonVariants = ['primary', 'success', 'danger', 'light', 'dark'] as const;
 const buttonSizes = ['xs', 'sm', 'md', 'lg'] as const;
 
-export const Sizes: React.FC = (): ReactElement => (
+const Template: Story<ButtonProps> = args => (
   <Box childGap="xl">
     {buttonSizes.map(size => (
       <Box childGap="sm" key={size}>
         <Box childGap="sm" direction="row">
           {buttonVariants.map(variant => (
-            <Button size={size} variant={variant} key={`${size}-${variant}`}>
-              {`${size} ${variant}`}
-            </Button>
-          ))}
-        </Box>
-        <Box childGap="sm" direction="row" key={size}>
-          {buttonVariants.map(variant => (
-            <Button size={size} variant={variant} isOutlined key={`${size}-${variant}-outline`}>
-              {`${size} ${variant}`}
-            </Button>
-          ))}
-        </Box>
-      </Box>
-    ))}
-    <Box childGap="sm">
-      <Button>Full Width</Button>
-      <Button fullWidth isOutlined>
-        Full Width Outline
-      </Button>
-    </Box>
-  </Box>
-);
-
-export const Loading: React.FC = (): ReactElement => (
-  <Box childGap="xl">
-    {buttonSizes.map(size => (
-      <Box childGap="sm" key={size}>
-        <Box childGap="sm" direction="row">
-          {buttonVariants.map(variant => (
-            <Button isLoading size={size} variant={variant} key={`${size}-${variant}`}>
+            <Button {...args} size={size} variant={variant} key={`${size}-${variant}`}>
               {`${size} ${variant}`}
             </Button>
           ))}
@@ -54,7 +25,7 @@ export const Loading: React.FC = (): ReactElement => (
         <Box childGap="sm" direction="row" key={size}>
           {buttonVariants.map(variant => (
             <Button
-              isLoading
+              {...args}
               size={size}
               variant={variant}
               isOutlined
@@ -67,92 +38,21 @@ export const Loading: React.FC = (): ReactElement => (
       </Box>
     ))}
     <Box childGap="sm">
-      <Button isLoading>Full Width</Button>
-      <Button isLoading fullWidth isOutlined>
+      <Button {...args}>Full Width</Button>
+      <Button {...args} fullWidth isOutlined>
         Full Width Outline
       </Button>
     </Box>
   </Box>
 );
 
-export const Disabled: React.FC = (): ReactElement => (
-  <Box childGap="xl">
-    {buttonSizes.map(size => (
-      <Box childGap="sm" key={size}>
-        <Box childGap="sm" direction="row">
-          {buttonVariants.map(variant => (
-            <Button isDisabled size={size} variant={variant} key={`${size}-${variant}`}>
-              {`${size} ${variant}`}
-            </Button>
-          ))}
-        </Box>
-        <Box childGap="sm" direction="row" key={size}>
-          {buttonVariants.map(variant => (
-            <Button
-              isDisabled
-              size={size}
-              variant={variant}
-              isOutlined
-              key={`${size}-${variant}-outline`}
-            >
-              {`${size} ${variant}`}
-            </Button>
-          ))}
-        </Box>
-      </Box>
-    ))}
-    <Box childGap="sm">
-      <Button isDisabled>Full Width</Button>
-      <Button isDisabled fullWidth isOutlined>
-        Full Width Outline
-      </Button>
-    </Box>
-  </Box>
-);
+export const Sizes = Template.bind({});
 
-export const WithIcons: React.FC = (): ReactElement => (
-  <Box childGap="md" display="block">
-    <Box direction="row" childGap="md">
-      <Button variant="primary" iconPrefix="mail">
-        Email
-      </Button>
-      <Button variant="primary" isOutlined iconSuffix="chat">
-        Chat Now
-      </Button>
-    </Box>
-    <Box direction="row" childGap="md">
-      <Button variant="light" iconPrefix="caret-left">
-        back
-      </Button>
-      <Button variant="light" iconSuffix="caret-right">
-        next
-      </Button>
-    </Box>
-    <Box direction="row" childGap="md">
-      <Button size="sm" variant="light" iconPrefix="caret-left">
-        back
-      </Button>
-      <Button size="sm" variant="light" iconSuffix="caret-right">
-        next
-      </Button>
-    </Box>
-    <Box direction="row" childGap="md">
-      <Button size="xs" variant="light" iconPrefix="caret-left">
-        back
-      </Button>
-      <Button size="xs" variant="light" iconSuffix="caret-right">
-        next
-      </Button>
-    </Box>
-    <Box>
-      <Button fullWidth variant="primary" iconPrefix="check">
-        check
-      </Button>
-    </Box>
-    <Box>
-      <Button fullWidth variant="primary" isOutlined iconSuffix="check">
-        check
-      </Button>
-    </Box>
-  </Box>
-);
+export const Loading = Template.bind({});
+Loading.args = { isLoading: true };
+
+export const Disabled = Template.bind({});
+Disabled.args = { isDisabled: true };
+
+export const WithIcons = Template.bind({});
+WithIcons.args = { iconPrefix: 'mail', iconSuffix: 'chat' };
