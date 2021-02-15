3e6c26907dc9e3164b85471ec96643813e01faa5
Eric Osborne <osbornedesign@yahoo.com>
2021-02-15 14:37:01 -0500

    docs: Heading component documentation re-organization (#398)
    * added Heading documentation
    
    * fixed lint-js issues
    
    * updated Visual Tests
    
    * fixed lint-js issues
    
    * updated to match prop names
    
    * Update src/components/Heading/Heading.Overview.stories.mdx
    
    * use single quotes and no parens around sole arrow func parameter
    
    Co-authored-by: Nathan Young <1447339+nathanyoung@users.noreply.github.com>

---
+++ src/components/Heading/Heading.VisualTests.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import Box from '../Box/Box';
+import { FONT_COLOR_OPTIONS, FONT_SIZE_OPTIONS } from '../../lib/tokens';
+import Heading, { HeadingProps } from './Heading';
+
+export default {
+  title: 'Components/Heading/Visual Regression Tests',
+  component: Heading,
+} as Meta;
+
+const SizeTemplate: Story<HeadingProps> = args => (
+  <Box>
+    {[...FONT_SIZE_OPTIONS].map(size => (
+      <Box>
+        <Heading {...args} size={size} />
+      </Box>
+    ))}
+  </Box>
+);
+
+const VariantTemplate: Story<HeadingProps> = args => (
+  <Box>
+    {[...FONT_COLOR_OPTIONS].reverse().map(variant => (
+      <Box>
+        <Box>
+          <Heading {...args} variant={variant} />
+        </Box>
+      </Box>
+    ))}
+  </Box>
+);
+
+export const Size = SizeTemplate.bind({});
+Size.args = {
+  children: 'Hello world!',
+};
+
+export const Variant = VariantTemplate.bind({});
+Variant.args = {
+  children: 'Hello world!',
+};
