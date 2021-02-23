329fa85b021c96d486f3f3d6cbc597f6943a911d
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2021-02-22 18:25:57 -0800

    docs(Spinner): add playground and visual regression test stories (#408)
    * docs(Spinner): add playground and visual regression test stories
    
    * Update Spinner.stories.mdx
    
    * update description

---
+++ src/components/Spinner/Spinner.Playground.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import Spinner, { SpinnerProps } from './Spinner';
+import { BRAND_COLOR_NAMES } from '../../lib/tokens';
+import { SPINNER_SIZES } from './Spinner.constants';
+
+export default {
+  title: 'Components/Spinner/Playground',
+  component: Spinner,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: BRAND_COLOR_NAMES,
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: SPINNER_SIZES,
+      },
+    },
+    className: {
+      control: 'text',
+    },
+  },
+} as Meta;
+
+const Template: Story<SpinnerProps> = ({ ...args }) => <Spinner {...args} />;
+
+export const Playground = Template.bind({});
+Playground.args = {
+  variant: 'primary',
+};
