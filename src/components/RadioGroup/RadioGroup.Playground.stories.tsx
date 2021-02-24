cf74e36e97d4801f36fe0edb1fe524b472972ee0
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2021-02-24 08:27:56 -0800

    RadioGroup

---
+++ src/components/RadioGroup/RadioGroup.Playground.stories.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import RadioGroup, { RadioGroupProps } from './RadioGroup';
+
+export default {
+  title: 'Components/RadioGroup/Playground',
+  component: RadioGroup,
+  argTypes: {
+    value: {
+      control: 'text',
+    },
+    error: {
+      control: 'text',
+    },
+    description: {
+      control: 'text',
+    },
+    isDisabled: {
+      control: 'boolean',
+    },
+    isRequired: {
+      control: 'boolean',
+    },
+    title: {
+      control: 'text',
+    },
+    name: {
+      control: 'text',
+    },
+    className: {
+      control: 'text',
+    },
+    direction: {
+      control: {
+        type: 'radio',
+        options: ['column', 'row'],
+      },
+    },
+    options: {
+      control: 'object',
+    },
+  },
+} as Meta;
+
+const Template: Story<RadioGroupProps> = ({ ...args }) => <RadioGroup {...args} />;
+
+export const Playground = Template.bind({});
+Playground.args = {
+  title: 'Playground Radiogroup',
+  description: 'Choose your favorite flavor',
+  name: 'playgroundRadioGroup',
+  direction: 'row',
+  value: 'chocolate',
+  options: [
+    {
+      id: 'chocolate',
+      value: 'chocolate',
+      label: 'Chocolate',
+    },
+    {
+      id: 'strawberry',
+      value: 'strawberry',
+      label: 'Strawberry',
+    },
+    {
+      id: 'vanilla',
+      value: 'vanilla',
+      label: 'Vanilla',
+    },
+  ],
+};
