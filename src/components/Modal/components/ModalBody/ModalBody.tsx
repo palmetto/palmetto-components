45c3aa0c4e195ab8d2586766bff6c0df449f2324
Juan Fabrega <juanantoniofabrega@gmail.com>
2021-03-23 13:43:29 -0400

    preventing positioning issue with overflowing dynamic height content

--- src/components/Modal/components/ModalBody/ModalBody.tsx
+++ src/components/Modal/components/ModalBody/ModalBody.tsx
@@ -1,15 +1,25 @@
 import React, { FC } from 'react';
 import { Box, BoxProps } from '../../../Box/Box';
 
-export type ModalBodyProps = Omit<BoxProps, 'as' | 'radius'>;
+export type ModalBodyProps = BoxProps;
 
 export const ModalBody: FC<ModalBodyProps> = ({
   children,
   flex = 'auto',
   padding = 'lg',
+  overflow = 'auto',
+  position = 'relative',
+  height = '100',
   ...restProps
 }) => (
-  <Box padding={padding} flex={flex} {...restProps}>
+  <Box
+    padding={padding}
+    flex={flex}
+    overflow={overflow}
+    position={position}
+    height={height}
+    {...restProps}
+  >
     {children}
   </Box>
 );
