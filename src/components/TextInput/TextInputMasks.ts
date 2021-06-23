a10e93e95e954e154b0c60e599afc42049ff63bb
Juan Fabrega <juanantoniofabrega@gmail.com>
2021-06-23 11:53:42 -0400

    make size prop responsive for textinput

--- src/components/TextInput/TextInputMasks.ts
+++ src/components/TextInput/TextInputMasks.ts
@@ -1,15 +1,50 @@
-export const phone = {
+import { InputMaskType } from './TextInput';
+
+export type PhoneMask = {
+  numericOnly: boolean;
+  blocks: number[];
+  delimiters: string[];
+};
+
+export type CreditCardMask = {
+  creditCard: boolean;
+};
+
+export type DateMask = {
+  date: boolean;
+  delimiter: string;
+  datePattern: string[];
+};
+
+export const phoneMask: PhoneMask = {
   numericOnly: true,
   blocks: [0, 3, 0, 3, 4],
   delimiters: ['(', ')', ' ', '-'],
 };
 
-export const creditCard = {
+export const creditCardMask: CreditCardMask = {
   creditCard: true,
 };
 
-export const date = {
+export const dateMask: DateMask = {
   date: true,
   delimiter: '-',
   datePattern: ['m', 'd', 'Y'],
 };
+
+const availableInputMaskTypes = {
+  phone: phoneMask,
+  creditCard: creditCardMask,
+  date: dateMask,
+};
+
+export const getInputMaskType = (
+  mask: InputMaskType,
+) => {
+  if (typeof mask === 'string') {
+    return availableInputMaskTypes[mask];
+  }
+
+  return mask;
+};
+
