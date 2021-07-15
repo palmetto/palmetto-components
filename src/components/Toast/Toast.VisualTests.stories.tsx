6fa85a4f3a61b2b95aee7542a37753f9ed99c8d3
Juan Fabrega <juanantoniofabrega@gmail.com>
2021-07-15 17:46:13 -0500

    typos

--- src/components/Toast/Toast.VisualTests.stories.tsx
+++ src/components/Toast/Toast.VisualTests.stories.tsx
@@ -67,42 +67,42 @@ export const CannotDismiss = (): React.ReactNode => {
 
 export const TopLeft = (): React.ReactNode => {
   setTimeout(() => {
-    toast('cannot dismiss toast', { position: 'top-left' });
+    toast('top left', { position: 'top-left' });
   }, 1);
   return <></>;
 };
 
 export const TopCenter = (): React.ReactNode => {
   setTimeout(() => {
-    toast('cannot dismiss toast', { position: 'top-center' });
+    toast('top center', { position: 'top-center' });
   }, 1);
   return <></>;
 };
 
 export const TopRight = (): React.ReactNode => {
   setTimeout(() => {
-    toast('cannot dismiss toast', { position: 'top-right' });
+    toast('top right', { position: 'top-right' });
   }, 1);
   return <></>;
 };
 
 export const BottomLeft = (): React.ReactNode => {
   setTimeout(() => {
-    toast('cannot dismiss toast', { position: 'bottom-left' });
+    toast('bottom left', { position: 'bottom-left' });
   }, 1);
   return <></>;
 };
 
 export const BottomCenter = (): React.ReactNode => {
   setTimeout(() => {
-    toast('cannot dismiss toast', { position: 'bottom-center' });
+    toast('bottom center', { position: 'bottom-center' });
   }, 1);
   return <></>;
 };
 
 export const BottomRight = (): React.ReactNode => {
   setTimeout(() => {
-    toast('cannot dismiss toast', { position: 'bottom-right' });
+    toast('bottom-right', { position: 'bottom-right' });
   }, 1);
   return <></>;
 };
