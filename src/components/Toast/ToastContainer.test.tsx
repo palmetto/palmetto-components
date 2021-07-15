75ea1501d6e703c3c8346ee0ee5b2cd2b82751a3
Juan Fabrega <juanantoniofabrega@gmail.com>
2021-07-15 17:36:06 -0500

    more tests

--- src/components/Toast/ToastContainer.test.tsx
+++ src/components/Toast/ToastContainer.test.tsx
@@ -3,6 +3,7 @@ import { screen, render, cleanup, within } from '@testing-library/react';
 import { ToastContainer } from './ToastContainer';
 import { ToastPosition } from '.';
 import { toast } from './toast';
+import { reject, resolve } from 'q';
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
@@ -100,6 +101,26 @@ describe('ToastContainer', () => {
       toast.custom(<p>test custom toast</p>);
 
       await expect(screen.getByText('test custom toast')).toBeInTheDocument();
+    });
+
+    test('async', async () => {
+      render(<ToastContainer />);
+      const myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (Date.now() % 2 === 0) {
+            resolve('yay');
+          } else {
+            reject('oh no!');
+          }
+        }, 1000);
+      });
+      toast.async(myPromise, {
+        loading: 'loading...',
+        success: (data) => `success ${data}`,
+        error: (err) => `error ${err}`,
+      });
+
+      await expect(screen.getByText('loading...')).toBeInTheDocument();
     });
 
     test('with children function', async () => {
