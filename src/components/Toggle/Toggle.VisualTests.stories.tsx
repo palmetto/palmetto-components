2dba04aa0f9b44990fab47f0b0d965339cc2e342
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2021-07-22 15:34:27 -0700

    lint

--- src/components/Toggle/Toggle.VisualTests.stories.tsx
+++ src/components/Toggle/Toggle.VisualTests.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { Toggle, ToggleProps } from './Toggle';
-import { TOGGLE_SIZES } from './Toggle.constants';
+import TOGGLE_SIZES from './Toggle.constants';
 import { Box } from '../Box/Box';
 
 export default {
