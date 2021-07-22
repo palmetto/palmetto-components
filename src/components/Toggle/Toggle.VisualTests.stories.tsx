40b3b3fcc8c2dd5d0b559846ef207924e6810ae0
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2021-07-22 15:26:15 -0700

    lint

--- src/components/Toggle/Toggle.VisualTests.stories.tsx
+++ src/components/Toggle/Toggle.VisualTests.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { Toggle, ToggleProps } from './Toggle';
 import { TOGGLE_SIZES } from './Toggle.constants';
