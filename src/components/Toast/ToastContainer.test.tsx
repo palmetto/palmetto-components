3f57f4ca495b033770f97e1aa5e5d405d9592cd0
Juan Fabrega <juanantoniofabrega@gmail.com>
2021-08-03 10:42:06 -0400

    programmatic dismissal story

--- src/components/Toast/ToastContainer.test.tsx
+++ src/components/Toast/ToastContainer.test.tsx
@@ -1,4 +1,11 @@
-import { act, cleanup, render, screen } from '@testing-library/react';
+import {
+  act,
+  cleanup,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import React from 'react';
 import { ToastPosition } from '.';
 import { toast } from './toast';
@@ -18,171 +25,197 @@ Object.defineProperty(window, 'matchMedia', {
   })),
 });
 
-afterEach(cleanup);
-
 describe('ToastContainer', () => {
-  test('Default', () => {
-    render(<ToastContainer data-testid="toast-container" />);
+  afterEach(() => {
+    cleanup();
+  });
 
-    expect(screen.getByTestId('toast-container')).toBeInTheDocument();
+  test('Default', () => {
+    render(<ToastContainer data-testid="toast-container-default" />);
+
+    expect(screen.getByTestId('toast-container-default')).toBeInTheDocument();
   });
 
   test('With Toasts', async () => {
     render(<ToastContainer data-testid="toast-container" />);
-
+    let toastId: string;
+  
     act(() => {
-      toast('test blank toast');
+      toastId = toast('test blank toast');
     });
 
-    await expect(screen.getByText('test blank toast')).toBeInTheDocument();
-  });
-
-  describe('Positions', () => {
-    const positions: ToastPosition[] = [
-      'top-left',
-      'top-right',
-      'top-center',
-      'bottom-left',
-      'bottom-center',
-      'bottom-right',
-    ];
-
-    positions.forEach(position => {
-      test(`${position}`, async () => {
-        render(<ToastContainer data-testid={`toast-container-${position}`} position={position} />);
-
-        act(() => {
-          toast(`test blank toast ${position}`);
-        });
-
-        expect(screen.getByTestId(`toast-container-${position}`)).toBeInTheDocument();
-
-        await expect(screen.getByText(`test blank toast ${position}`)).toBeInTheDocument();
-
-        const verticalStyle: React.CSSProperties = position.includes('top') ? { top: 0 } : { bottom: 0 };
-        const horizontalStyle = {
-          ...position.includes('center') && { justifyContent: 'center' },
-          ...(!position.includes('center') && position.includes('right')) && { justifyContent: 'flex-end' },
-        };
-
-        expect(screen.getByTestId(`toast-container-${position}`).children[0]).toHaveStyle({
-          ...verticalStyle,
-          ...horizontalStyle,
-        });
-      });
+    await waitFor(() => {
+      expect(screen.getByText('test blank toast')).toBeInTheDocument();
     });
 
-    positions.forEach(position => {
-      test(`Custom toast positions ${position}`, async () => {
-        render(<ToastContainer />);
-
-        act(() => {
-          toast(`test custom position toast ${position}`, { position });
-        });
-
-        await expect(screen.getByText(`test custom position toast ${position}`)).toBeInTheDocument();
-      });
-    });
-  });
-
-  describe('Toast Types', () => {
-    test('Success', async () => {
-      render(<ToastContainer />);
-
-      act(() => {
-        toast.success('test success toast');
-      });
-
-      await expect(screen.getByText('test success toast')).toBeInTheDocument();
+    await act(async () => {
+      console.log('TOAST ID', toastId);
+      await toast.dismiss(toastId);
     });
 
-    test('error', async () => {
-      render(<ToastContainer />);
+    await waitForElementToBeRemoved(() => screen.getByText('test blank toast'), { timeout: 15000 });
 
-      act(() => {
-        toast.error('test error toast');
-      });
+    //await waitFor(() => { expect(screen.getByText('test blank toast')).not.toBeInTheDocument() });
+  }, 20000);
 
-      await expect(screen.getByText('test error toast')).toBeInTheDocument();
-    });
+  // describe('Positions', () => {
+  //   const positions: ToastPosition[] = [
+  //     'top-left',
+  //     'top-right',
+  //     'top-center',
+  //     'bottom-left',
+  //     'bottom-center',
+  //     'bottom-right',
+  //   ];
 
-    test('Loading', async () => {
-      render(<ToastContainer />);
+  //   positions.forEach(position => {
+  //     test(`${position}`, async () => {
+  //       render(<ToastContainer data-testid={`toast-container-${position}`} position={position} />);
 
-      act(() => {
-        toast.loading('test loading toast');
-      });
+  //       let toastId: string;
 
-      await expect(screen.getByText('test loading toast')).toBeInTheDocument();
-    });
+  //       act(() => {
+  //         toastId = toast(`test position toast ${position}`);
+  //       });
 
-    test('Custom', async () => {
-      render(<ToastContainer />);
+  //       expect(screen.getByTestId(`toast-container-${position}`)).toBeInTheDocument();
 
-      act(() => {
-        toast.custom(<p>test custom toast</p>);
-      });
+  //       await expect(screen.getByText(`test position toast ${position}`)).toBeInTheDocument();
 
-      await expect(screen.getByText('test custom toast')).toBeInTheDocument();
-    });
+  //       const verticalStyle: React.CSSProperties = position.includes('top') ? { top: 0 } : { bottom: 0 };
+  //       const horizontalStyle = {
+  //         ...position.includes('center') && { justifyContent: 'center' },
+  //         ...(!position.includes('center') && position.includes('right')) && { justifyContent: 'flex-end' },
+  //       };
 
-    test('Async', async () => {
-      render(<ToastContainer />);
-      const myPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (Date.now() % 2 === 0) {
-            resolve('yay');
-          } else {
-            // eslint-disable-next-line
-            reject('oh no!');
-          }
-        }, 1000);
-      });
+  //       expect(screen.getByTestId(`toast-container-${position}`).children[0]).toHaveStyle({
+  //         ...verticalStyle,
+  //         ...horizontalStyle,
+  //       });
 
-      act(() => {
-        toast.async(myPromise, {
-          loading: 'loading...',
-          success: data => `success ${data}`,
-          error: err => `error ${err}`,
-        });
-      });
+  //     //   act(() => {
+  //     //     toast.dismiss(toastId);
+  //     //   });
 
-      await expect(screen.getByText('loading...')).toBeInTheDocument();
-    });
+  //     //   await expect(screen.getByText(`test blank toast ${position}`)).not.toBeInTheDocument();
+  //     });
+  //   });
 
-    test('With children function', async () => {
-      render(<ToastContainer />);
+  //   positions.forEach(position => {
+  //     test(`Custom toast positions ${position}`, async () => {
+  //       render(<ToastContainer />);
 
-      act(() => {
-        toast(t => (
-          <span>
-            Custom and
-            <b>bold</b>
-            <button type="button" onClick={() => toast.dismiss(t.id)}>Dismiss</button>
-          </span>
-        ));
-      });
+  //       act(() => {
+  //         toast(`test custom position toast ${position}`, { position });
+  //       });
 
-      await expect(screen.getByText('Custom and')).toBeInTheDocument();
-    });
-  });
+  //       await expect(screen.getByText(`test custom position toast ${position}`)).toBeInTheDocument();
+  //     });
+  //   });
+  // });
 
-  test('Compact', async () => {
-    render(<ToastContainer data-testid="toast-container" />);
+  // describe('Toast Types', () => {
+  //   test('Success', async () => {
+  //     render(<ToastContainer />);
 
-    act(() => {
-      toast('test compact toast', { isCompact: true });
-    });
+  //     act(() => {
+  //       toast.success('test success toast');
+  //     });
 
-    await expect(screen.getByText('test compact toast')).toBeInTheDocument();
-  });
+  //     await expect(screen.getByText('test success toast')).toBeInTheDocument();
+  //   });
+
+  //   test('error', async () => {
+  //     render(<ToastContainer />);
+
+  //     act(() => {
+  //       toast.error('test error toast');
+  //     });
+
+  //     await expect(screen.getByText('test error toast')).toBeInTheDocument();
+  //   });
+
+  //   test('Loading', async () => {
+  //     render(<ToastContainer />);
+
+  //     act(() => {
+  //       toast.loading('test loading toast');
+  //     });
+
+  //     await expect(screen.getByText('test loading toast')).toBeInTheDocument();
+  //   });
+
+  //   test('Custom', async () => {
+  //     render(<ToastContainer />);
+
+  //     act(() => {
+  //       toast.custom(<p>test custom toast</p>);
+  //     });
+
+  //     await expect(screen.getByText('test custom toast')).toBeInTheDocument();
+  //   });
+
+  //   test('Async', async () => {
+  //     render(<ToastContainer />);
+  //     const myPromise = new Promise((resolve, reject) => {
+  //       setTimeout(() => {
+  //         if (Date.now() % 2 === 0) {
+  //           resolve('yay');
+  //         } else {
+  //           // eslint-disable-next-line
+  //           reject('oh no!');
+  //         }
+  //       }, 1000);
+  //     });
+
+  //     act(() => {
+  //       toast.async(myPromise, {
+  //         loading: 'loading...',
+  //         success: data => `success ${data}`,
+  //         error: err => `error ${err}`,
+  //       });
+  //     });
+
+  //     await expect(screen.getByText('loading...')).toBeInTheDocument();
+  //   });
+
+  //   test('With children function', async () => {
+  //     render(<ToastContainer />);
+
+  //     act(() => {
+  //       toast(t => (
+  //         <span>
+  //           Custom and
+  //           <b>bold</b>
+  //           <button type="button" onClick={() => toast.dismiss(t.id)}>Dismiss</button>
+  //         </span>
+  //       ));
+  //     });
+
+  //     await expect(screen.getByText('Custom and')).toBeInTheDocument();
+  //   });
+  // });
+
+  // test('Compact', async () => {
+  //   render(<ToastContainer data-testid="toast-container" />);
+
+  //   act(() => {
+  //     toast('test compact toast', { isCompact: true });
+  //   });
+
+  //   await expect(screen.getByText('test compact toast')).toBeInTheDocument();
+  // });
 
   // test('Dismissing Toast', async () => {
   //   render(<ToastContainer data-testid="toast-container" />);
 
-  //   toast('dismissing toast');
+  //   act(() => {
+  //     toast('dismissing toast');
+  //   });
 
-  //   const toastElement =  await screen.getByText('dismissing toast');
-  //   console.log(toastElement);
+  //   const closeIcon =  await screen.getByTestId('icon-testid--remove-light');
+  //   const closeButton = closeIcon.closest('button');
+  //   console.log(closeButton);
+  //   console.log(screen);
   // });
 });
