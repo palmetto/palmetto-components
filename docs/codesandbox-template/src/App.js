8835f44eb865b909699d7c77ec54ededeb3b7c1c
Juan Fabrega <juanantoniofabrega@gmail.com>
2021-11-04 11:16:38 -0400

    codesandbox stories

---
+++ docs/codesandbox-template/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MyComponent from './MyComponent';
+import '@palmetto/palmetto-components/dist/css/reset.css';
+import '@palmetto/palmetto-components/dist/css/utilities.css';
+import '@palmetto/palmetto-components/dist/css/variables.css';
+import '@palmetto/palmetto-components/dist/css/index.css';
+import '@palmetto/palmetto-components/dist/css/fonts.css';
+
+export default function App() {
+  return <MyComponent />;
+}
