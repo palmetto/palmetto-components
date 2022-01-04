31b61773a7af28eec74255f9138ef63fef54844b
Juan Fabrega <juanantoniofabrega@gmail.com>
2022-01-04 12:25:30 -0500

    wrap up tests

--- src/components/CategoryFilter/CategoryFilter.tsx
+++ src/components/CategoryFilter/CategoryFilter.tsx
@@ -1,36 +1,45 @@
 import React, { forwardRef, ForwardRefExoticComponent } from 'react';
+import classNames from 'classnames';
 import { Box, BoxProps } from '../Box/Box';
+import { Icon } from '../Icon/Icon';
 import { ResponsiveProp } from '../../types';
+import styles from './CategoryFilter.module.scss';
 
 export type BaseCategoryFilterSize = 'xs' | 'sm' | 'md' | 'lg';
 
 export interface CategoryFilterProps extends BoxProps {
   isSelected: boolean;
-  size: BaseCategoryFilterSize | ResponsiveProp<BaseCategoryFilterSize>;
+  isDisabled?: boolean;
+  size?: BaseCategoryFilterSize | ResponsiveProp<BaseCategoryFilterSize>;
 }
 
 type ResponsiveCategoryFilterProps = {
   fontSize: BoxProps['fontSize'];
   padding: BoxProps['padding'];
+  childGap: BoxProps['childGap'];
 }
 
 const propSizeMap: { [key in BaseCategoryFilterSize]: ResponsiveCategoryFilterProps} = {
   xs: {
     fontSize: 'xs',
-    padding: '0',
+    padding: '2xs',
+    childGap: '2xs',
   },
   sm: {
     fontSize: 'sm',
     padding: '2xs xs',
+    childGap: '2xs',
   },
   md: {
     fontSize: 'md',
     padding: 'sm md',
+    childGap: 'xs',
   },
   lg: {
     fontSize: 'lg',
     padding: 'sm md',
-  }
+    childGap: 'xs',
+  },
 };
 
 /**
@@ -40,44 +49,66 @@ const propSizeMap: { [key in BaseCategoryFilterSize]: ResponsiveCategoryFilterPr
  * @param prop Property map to use to map each breakpoint value.
  * @returns A mapping of responsive prop objects to their corresponding values per prop
  */
- export const computedResponsiveSize = ( // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
+export const computedResponsiveSize = ( // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
   size: CategoryFilterProps['size'],
-  prop: 'fontSize' | 'padding',
+  prop: 'fontSize' | 'padding' | 'childGap',
 ) => {
   if (size && !(typeof size === 'string') && typeof size === 'object') {
     return Object.entries(size)
       .reduce((acc, [key, value]) => ({ ...acc, [key]: propSizeMap[value][prop] }), {});
   }
 
-  return propSizeMap[size][prop] as string;
+  return propSizeMap[size || 'md'][prop] as string;
 };
 
-export const CategoryFilter: ForwardRefExoticComponent<CategoryFilterProps> = forwardRef<HTMLDivElement, CategoryFilterProps>((
+type CategoryFilterComponent = ForwardRefExoticComponent<CategoryFilterProps>;
+
+export const CategoryFilter: CategoryFilterComponent = forwardRef<HTMLDivElement, CategoryFilterProps>((
   {
     children,
     isSelected,
+    isDisabled = false,
+    className = undefined,
     size = 'md',
     ...restProps
   },
   ref,
 ) => {
+  const classes = classNames(
+    styles['category-filter'],
+    className,
+    {
+      [styles.disabled]: isDisabled,
+      [styles.selected]: isSelected,
+    },
+  );
+
   return (
     <Box
       as="button"
-      aria-role="checkbox"
-      background="transparent"
-      hover={{
-        background: 'grey-100',
-      }}
-      radius="lg"
-      borderColor="grey-100"
+      role="checkbox"
+      alignItems="center"
+      disabled={isDisabled}
+      background={isSelected ? 'primary' : 'transparent'}
+      className={classes}
+      childGap={computedResponsiveSize(size, 'childGap')}
+      direction="row"
+      hover={!isDisabled ? {
+        background: isSelected ? 'primary-600' : 'grey-100',
+      } : undefined}
+      cursor={isDisabled ? 'not-allowed' : 'pointer'}
+      radius="xl"
+      borderColor={isSelected ? 'primary' : 'grey-100'}
       borderWidth="xs"
       aria-checked={isSelected}
       fontSize={computedResponsiveSize(size, 'fontSize')}
+      fontWeight="bold"
+      color={isSelected ? 'white' : 'dark'}
       padding={computedResponsiveSize(size, 'padding')}
       ref={ref}
       {...restProps}
     >
+      {!!isSelected && <Icon name="check" />}
       {children}
     </Box>
   );
