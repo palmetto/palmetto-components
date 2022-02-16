ac910d4612e2c60eb7378c7dbef18a69fac0f71f
Taylor Blanton <matthewtblanton@gmail.com>
2022-02-16 16:29:04 -0600

    feat(Button): add white button variant (#598)
    * add white button variant
    
    * missed adding a visual test for the white button
    
    * Update src/styles/variables/buttons.scss
    
    * refactor for theming
    
    * fix
    
    * Update Button.tsx
    
    * add bg change on hover
    
    * document tokens
    
    * whitespace
    
    Co-authored-by: Nathan Young <1447339+nathanyoung@users.noreply.github.com>
    Co-authored-by: juanfabrega <juanantoniofabrega@gmail.com>

--- src/components/Button/Button.constants.ts
+++ src/components/Button/Button.constants.ts
@@ -1,5 +1,5 @@
 import { ButtonVariant, ButtonSize } from './Button';
 
-export const BUTTON_VARIANTS: ButtonVariant[] = ['primary', 'danger', 'light', 'dark', 'success'];
+export const BUTTON_VARIANTS: ButtonVariant[] = ['primary', 'danger', 'light', 'dark', 'success', 'white'];
 
 export const BUTTON_SIZES: ButtonSize[] = ['xs', 'sm', 'md', 'lg'];
