52f25a0fbee7ff4e7c2853ec54b27593579ceca5
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2022-02-21 19:35:05 -0800

    fix textarea sizes

--- src/components/TextareaInput/TextareaInput.tsx
+++ src/components/TextareaInput/TextareaInput.tsx
@@ -7,6 +7,7 @@ import { Box, BoxProps } from '../Box/Box';
 import { FormLabel } from '../FormLabel/FormLabel';
 import { InputValidationMessage } from '../InputValidationMessage/InputValidationMessage';
 import { getAutoCompleteValue } from '../../lib/getAutoCompleteValue';
+import { generateResponsiveClasses } from '../../lib/generateResponsiveClasses';
 import { computedResponsiveSize } from './TextareaInputSizeUtilities'; // eslint-disable-line import/no-cycle
 import styles from './TextareaInput.module.scss';
 
@@ -123,9 +124,12 @@ export const TextareaInput: FC<TextareaInputProps> = ({
   size = 'md',
   ...restProps
 }) => {
+  const responsiveClasses = generateResponsiveClasses('size', size);
+
   const inputWrapperClasses = classNames(
     'palmetto-components__variables__form-control',
     styles['textarea-input-wrapper'],
+    ...responsiveClasses.map(c => (styles[c])),
     {
       [styles.error]: error,
       [styles.disabled]: isDisabled,
