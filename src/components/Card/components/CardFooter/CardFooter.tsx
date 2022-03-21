ba5b4c1cf4a1d0e3be54fdaf353963ee2f8bbe78
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2022-03-21 16:30:03 -0700

    enable Card.Footer customization through props

--- src/components/Card/components/CardFooter/CardFooter.tsx
+++ src/components/Card/components/CardFooter/CardFooter.tsx
@@ -1,6 +1,7 @@
 import React, { FC, ReactNode } from 'react';
 import classNames from 'classnames';
 import { Box, BoxProps } from '../../../Box/Box';
+import { BrandColor } from '../../../../types';
 import styles from '../../Card.module.scss';
 
 export interface CardFooterProps extends BoxProps {
@@ -9,26 +10,43 @@ export interface CardFooterProps extends BoxProps {
    */
   children?: ReactNode;
   /**
-   * Additional props to be spread to rendered element
+   * If defined as a prop, all theme styling will be removed.
+   * Any valid [brand color token](/?path=/story/design-tokens-design-tokens--page#color), or a `url()` for an image
    */
-  [x: string]: any; // eslint-disable-line
+  background?: BrandColor;
+  /**
+   * If defined as a prop, all theme styling will be removed.
+   * Any valid [brand color token](/?path=/story/design-tokens-design-tokens--page#color) for the border color
+   * Or a responsive prop with BrandColor for each breakpoint.
+   */
+  borderColor?: BrandColor;
 }
 
 export const CardFooter: FC<CardFooterProps> = ({
+  background = undefined,
+  borderColor = undefined,
   borderWidth = 'xs 0 0 0',
   children = null,
   className,
   display = 'block',
   padding = 'md lg',
   ...restProps
-}) => (
-  <Box
-    className={classNames(styles['card-footer'], className)}
-    display={display}
-    padding={padding}
-    borderWidth={borderWidth}
-    {...restProps}
-  >
-    {children}
-  </Box>
-);
+}) => {
+  const useTheme = background === undefined && borderColor === undefined;
+
+  const classes = classNames({ [styles['card-footer']]: useTheme }, className);
+
+  return (
+    <Box
+      className={classes}
+      display={display}
+      padding={padding}
+      background={background}
+      borderColor={borderColor}
+      borderWidth={borderWidth}
+      {...restProps}
+    >
+      {children}
+    </Box>
+  );
+};
