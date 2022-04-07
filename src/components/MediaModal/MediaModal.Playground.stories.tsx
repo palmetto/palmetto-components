032bb30593cbadd467c01c0768d14cadce7535a1
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2022-04-07 08:26:48 -0700

    Update MediaModal.Playground.stories.tsx

--- src/components/MediaModal/MediaModal.Playground.stories.tsx
+++ src/components/MediaModal/MediaModal.Playground.stories.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { MediaModal, MediaModalProps } from './MediaModal';
+import { Button } from '../Button/Button';
 
 export default {
   title: 'Components/MediaModal/Playground',
   component: MediaModal,
   argTypes: {
-    isOpen: {
-      control: 'boolean',
-    },
     title: {
       control: 'text',
     },
@@ -18,18 +16,30 @@ export default {
     footerContent: {
       control: 'text',
     },
+    closeButton: {
+      control: 'boolean',
+    },
   },
 } as Meta;
 
-const Template: Story<MediaModalProps> = ({ ...args }) => (
-  <MediaModal {...args}>
-    <img src="/images/landscape-mediamodal.jpg" alt="landscape test" />
-  </MediaModal>
-);
+const Template: Story<MediaModalProps> = ({ ...args }) => {
+  const [showModal, setShowModal] = React.useState(false);
+  const open = () => setShowModal(true);
+  const close = () => setShowModal(false);
+  return (
+    <>
+      <Button variant="light" onClick={open}>
+        Show MediaModal
+      </Button>
+      <MediaModal {...args} isOpen={showModal} onDismiss={close}>
+        <img src="/images/landscape-mediamodal.jpg" alt="landscape test" />
+      </MediaModal>
+    </>
+  );
+};
 
 export const Playground = Template.bind({});
 Playground.args = {
-  isOpen: false,
   title: 'title',
   description: 'description',
   footerContent: 'footer',
