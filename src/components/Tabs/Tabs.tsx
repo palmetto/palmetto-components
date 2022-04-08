7f87fce5cbeef24e665e6738eac0eadf0ce934f0
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2022-04-07 20:24:20 -0700

    Update src/components/Tabs/Tabs.tsx

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { ResponsiveProp } from '../../types';
 import { ButtonVariant } from '../Button/Button';
