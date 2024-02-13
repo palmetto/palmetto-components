cd6c3a9802beadf66cb5bcca310fa6fa0637f74c
nathanyoung <1447339+nathanyoung@users.noreply.github.com>
2024-02-13 09:12:40 -0800

    fix(Button): loading state icon color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -185,6 +185,14 @@ export const Button = forwardRef<
     };
 
     const getSpinnerVariant = () => {
+      if (tone === 'primary' && (variant === 'secondary' || variant === 'tertiary')) {
+        return 'primary';
+      }
+
+      if (tone === 'danger' && (variant === 'secondary' || variant === 'tertiary')) {
+        return 'danger';
+      }
+
       if (variant === 'primary' && (tone === 'primary' || tone === 'danger')) {
         return 'white';
       }
